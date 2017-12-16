19d0b0f920c27677da2e6d03dec00049e75265f2
BARTEK <cwiartek@interia.pl>
2017-12-16 12:25:08 +0100

    jeszcze jedna metoda

--- hello2.java
+++ hello2.java
@@ -2,6 +2,12 @@ class Hello {
 
 	public static void main(String[] args) {
 	
-	System.out.println("hello") 
+	System.out.println("hello");
+	}
+	
+	public void ddd {
+		
+		System.out.println("ccc");
+
 	}
 }
